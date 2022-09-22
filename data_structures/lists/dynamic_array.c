ac2540a9b568cb20ccd649c39cf4f5208aceb3b9
Antoni Shtipliyski <a.shtipliyski@nullone.com>
2022-09-22 15:00:09 +0100

    Add dynamic array skeleton

---
+++ data_structures/lists/dynamic_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+typedef struct darray_s {
+  int size;
+  int *data;
+} darray_t;
+
+darray_t* create_darray(int size) {
+  return NULL;
+}
+
+void darray_append(darray_t *arr, int val);
+int darray_pop(darray_t *arr);
+
+int darray_get(int i);
+void darray_set(int i, int val);
+
+int main () {
+
+  
+  
+  printf("hi\n");
+}
